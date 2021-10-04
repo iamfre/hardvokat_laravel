54294c2ad7706219d597789aa0fdc2a43e87e9a0
iamfree <zycs@yandex.ru>
2021-10-05 02:22:34 +0500

    fixed relationships category & service

--- database/migrations/2021_09_22_164030_create_services_table.php
+++ database/migrations/2021_09_22_164030_create_services_table.php
@@ -16,7 +16,7 @@ class CreateServicesTable extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->integer('category_id');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->unsignedInteger('price');
             $table->timestamps();
         });
